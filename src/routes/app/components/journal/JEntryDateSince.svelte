28a1c262c53b3c8b0177d70f18d41db007d3c7b1
Juande Martos <juande@bumxu.com>
2026-02-22 20:59:42 +0100

    Botón de actualizar siempre visible

--- src/routes/app/components/journal/JEntryDateSince.svelte
+++ src/routes/app/components/journal/JEntryDateSince.svelte
@@ -1,20 +1,20 @@
 <script lang="ts">
-    import Ic from '$lib/components/Ic.svelte';
-    import JDateTime from '$lib/components/JDateTime.svelte';
-    import type { EntrySchema } from '$lib/types/j4_types';
-    import { DateTime } from 'luxon';
-    import type { SvelteComponent } from 'svelte';
-    import { appStore } from '../../appstate.store.svelte';
+    import Ic from "$lib/components/Ic.svelte";
+    import JDateTime from "$lib/components/JDateTime.svelte";
+    import type { EntrySchema } from "$lib/types/j4_types";
+    import { DateTime } from "luxon";
+    import type { SvelteComponent } from "svelte";
+    import { appStore } from "../../appstate.store.svelte";
 
     type Props = {
         /** La entrada del diario a la que pertenece la fecha. */
         entry: EntrySchema;
         /** Un listener que se ejecutará cuando se modifique cualquiera de los campos. Opcional. */
-        onchange?: () => void,
+        onchange?: () => void;
         /** Un listener que se ejecutará cuando se modifique el campo fecha inicial. Opcional. */
-        onchangedatesince?: () => void,
+        onchangedatesince?: () => void;
         /** Un listener que se ejecutará cuando se modifique el campo fecha de actualización. Opcional. */
-        onchangedateupdated?: () => void,
+        onchangedateupdated?: () => void;
     };
     let { entry, onchange, onchangedatesince, onchangedateupdated }: Props = $props();
 
@@ -22,7 +22,8 @@
     let svDateSince: SvelteComponent;
     /** La fecha de actualización se muestra la tecla Ctrl está presionada, o alguna de ellas tiene el foco. */
     let showDateUpdated = $derived(
-        appStore.ctrlKeyPressed || appStore.focusedElement?.closest('.x-date-updated') != null);
+        appStore.ctrlKeyPressed || appStore.focusedElement?.closest(".x-date-updated") != null,
+    );
 
     /** Indica si el campo de fecha inicial tiene el foco. */
     let dateSinceFocused = $state(false);
@@ -34,7 +35,7 @@
     /** Al hacer click en icono junto a la fecha de actualización, la actualiza a la fecha y hora actuales. */
     function handleClickUpdateDateUpdated(ev: MouseEvent) {
         ev.preventDefault();
-        entry.dateUpdated = DateTime.local().toFormat('yyyyMMddHHmm');
+        entry.dateUpdated = DateTime.local().toFormat("yyyyMMddHHmm");
     }
 
     /** Al hacer click en el espacio vacío de la celda, se enfocará el campo de fecha inicial. */
@@ -55,7 +56,6 @@
         if (onchangedatesince) onchangedatesince();
         if (onchange) onchange();
     }
-
 </script>
 
 <div class="x-cmp" class:x-complex-design={useComplexDesign} role="none" onclick={handleClickCell}>
@@ -64,29 +64,32 @@
             <i class="fas fa-fw fa-xs fa-plus"></i>
         </div>
         <div class="flex-1">
-            <JDateTime bind:value={entry.dateSince}
-                       bind:this={svDateSince}
-                       onchange={handleChangeDateSince}
-                       onfocus={() => dateSinceFocused = true}
-                       onblur={() => dateSinceFocused = false} />
+            <JDateTime
+                bind:value={entry.dateSince}
+                bind:this={svDateSince}
+                onchange={handleChangeDateSince}
+                onfocus={() => (dateSinceFocused = true)}
+                onblur={() => (dateSinceFocused = false)}
+            />
         </div>
     </div>
-    {#if showDateUpdated}
-        <div class="x-date-wrapper x-date-updated flex items-center" class:x-focused={dateUpdatedFocused}>
-            <Ic class="flex-auto"
-                iconclass="far fa-fw fa-xs fa-pen"
-                iconclasshover="fas fa-fw fa-xs fa-arrows-rotate"
-                inheritcolor={true}
-                label="Establecer fecha y hora actuales"
-                onclick={handleClickUpdateDateUpdated} />
-            <div class="flex-1">
-                <JDateTime bind:value={entry.dateUpdated}
-                           onchange={handleChangeDateUpdated}
-                           onfocus={() => dateUpdatedFocused = true}
-                           onblur={() => dateUpdatedFocused = false} />
-            </div>
+    <div class="x-date-wrapper x-date-updated flex items-center" class:x-focused={dateUpdatedFocused}>
+        <Ic
+            iconclass="far fa-fw fa-xs fa-pen"
+            iconclasshover="fas fa-fw fa-xs fa-timer"
+            inheritcolor={true}
+            label="Establecer fecha y hora actuales"
+            onclick={handleClickUpdateDateUpdated}
+        />
+        <div class="flex-1" style:visibility={showDateUpdated ? "inherit" : "hidden"}>
+            <JDateTime
+                bind:value={entry.dateUpdated}
+                onchange={handleChangeDateUpdated}
+                onfocus={() => (dateUpdatedFocused = true)}
+                onblur={() => (dateUpdatedFocused = false)}
+            />
         </div>
-    {/if}
+    </div>
 </div>
 
 <style lang="scss">
@@ -94,7 +97,8 @@
         height: 100%;
         cursor: text;
 
-        &.x-complex-design, &:hover {
+        &.x-complex-design,
+        &:hover {
             .x-date-wrapper {
                 border-bottom: 1px dotted var(--table-sep-color);
             }
@@ -104,9 +108,12 @@
     .x-date-wrapper {
         text-wrap: nowrap;
         line-height: 13px;
-        transition: background-color 0.2s, color 0.2s;
+        transition:
+            background-color 0.2s,
+            color 0.2s;
         color: #333;
         padding: 0 2px;
+        border-bottom: 1px solid transparent;
 
         &:hover {
             background-color: var(--color-hovered);
@@ -120,7 +127,8 @@
     .x-date-updated {
         color: #ddd;
 
-        &.x-focused, &:hover {
+        &.x-focused,
+        &:hover {
             color: #aaa;
         }
     }
